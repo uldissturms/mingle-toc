23f88cacf3350e7ddddde7540ba406b51f471767
Uldis Sturms <tw.at.mrk+us@gmail.com>
2015-09-03 15:43:49 +0100

    Made mingle configurable - base url, doing on, done on and done state so that it can be used by teams.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Sample app can be found here: https://mingle-toc.herokuapp.com/.
+
+Set these environment variables to work with your mingle project:
+MINGLE_BASE_URL='http://<username>:<password>@<host>[:<port>]/api/v2/projects/markel_retail_transformation/'
+DOING_ON_STATE='Doing'
+DONE_ON_STATE='Done'
+DONE_STATE='Done'
--- cycleTimes.js
+++ cycleTimes.js
@@ -1,34 +1,41 @@
 let request = require('superagent');
 let _ = require('lodash');
 
-let mingleBaseUrl = 'https://integration-test:test-5@uldissturms.mingle.thoughtworks.com/api/v2/';
+let mingleBaseUrl = process.env.MINGLE_BASE_URL || 'https://integration-test:test-5@uldissturms.mingle.thoughtworks.com/api/v2/projects/test/';
+let doingOn = process.env.DOING_ON_STATE || 'Doing';
+let doneOn = process.env.DONE_ON_STATE || 'Done';
+let doneState = process.env.DONE_STATE || 'Done';
 
-let url = mingleBaseUrl + 'projects/test/cards/execute_mql.json?mql=SELECT Number,Name,Created_On,Doing,Done WHERE Status=Done';
+let compileMingleQuery = function(){  
+  return mingleBaseUrl + "cards/execute_mql.json?mql=SELECT Number,Name,Created_On,'" + doingOn + "','" + doneOn + "' WHERE Status=" + doneState + " ORDER BY '" + doneOn + "' DESC";
+}
 
-let diffInDays = function(from, to) {
+let diffInDays = function(from, to){
   const msInADay = 1000*60*60*24;
   return Math.round((to-from)/msInADay, 0);
 }
 
-let calculateCycleTimes = function(stories) {
+let calculateCycleTimes = function(stories){
   return _.map(stories, function(s){ return { 
     name: s.Name,
     number: s.Number,
-    leadTime: diffInDays(Date.parse(s['Created on']), Date.parse(s['Done'])),
-    cycleTime: diffInDays(Date.parse(s['Doing']), Date.parse(s['Done']))
+    leadTime: diffInDays(Date.parse(s['Created on']), Date.parse(s[doneOn])),
+    cycleTime: diffInDays(Date.parse(s[doingOn]), Date.parse(s[doneOn]))
     };
   });
 }
 
+let retrieve = function(cb){
+request
+  .get(compileMingleQuery())
+  .end(function(err, res){
+    let cycleTimes = calculateCycleTimes(res.body); 
+    cb({ stories: cycleTimes }, err);
+  });
+};
+
 let cycleTimes = {
-  retrieve: function(cb){
-    request
-      .get(url)
-      .end(function(err, res){
-        let cycleTimes = calculateCycleTimes(res.body); 
-        cb({ stories: cycleTimes }, err);
-      });
-  }
+  retrieve: retrieve
 };
 
 module.exports = cycleTimes;
